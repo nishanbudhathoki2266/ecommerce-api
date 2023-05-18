5cd315d28755c1b84c6361a5df7908a509e2eb91
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-18 09:42:55 +0545

    Added some dummy routes and controllers

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require('express');
 
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
+
+// JSON parser middleware with size limit of 10kb
+app.use(express.json({ limit: '10kb' }));
+
+
+// middlewares for serving static files
+app.use(express.static(`${__dirname}/public`));
+
+app.use('/api/v1/users', userRouter);
+
 
 module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+
+exports.getAllUsers = (req, res, next) => {
+    res.status(200).json({
+        message: "SUCCESS"
+    })
+}
+
+exports.createUser = (req, res, next) => {
+    res.status(200).json({
+        message: "SUCCESS"
+    })
+}
+exports.getUser = (req, res, next) => {
+    res.status(200).json({
+        message: "SUCCESS"
+    })
+}
+exports.deleteUser = (req, res, next) => {
+    res.status(200).json({
+        message: "SUCCESS"
+    })
+}
