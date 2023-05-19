e10cca58161f3bdd599d110c68c5e2191e4a5d87
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-19 11:17:54 +0545

    Added a sign up functionality

---
+++ controllers/authController.js
@@ -0,0 +1,39 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
+
+const createAndSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+
+    // Removing password from the output
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    })
+}
+
+exports.signup = catchAsync(async (req, res, next) => {
+    // Only take things that are required to create a user, not letting roles to be specified the user itself 
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    })
+
+    createAndSendToken(newUser, 201, res);
+
+})
