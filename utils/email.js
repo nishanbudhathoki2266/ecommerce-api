7ec69b4afa52586e4c6463dc183bc2e9a83c8019
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-20 19:19:24 +0545

    Fixed a typo of email sending

--- utils/email.js
+++ utils/email.js
@@ -4,7 +4,7 @@ const sendEmail = async (options) => {
     // Create a transporter
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
-        port: process.env.PORT,
+        port: process.env.EMAIL_PORT,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
