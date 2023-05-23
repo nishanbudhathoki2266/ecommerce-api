891dbb6c9b0513e2b440950532617238b5f20308
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-23 12:28:23 +0545

    Added a banner model

---
+++ models/bannerModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+const bannerSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    link: String,
+    status: {
+        type: String,
+        enum: {
+            values: ['active', 'inactive'],
+            message: "The banner status must be either active or inactive!"
+        }
+    },
+    createdBy: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    }
+})
+
+
+const Banner = mongoose.model('Banner', bannerSchema);
+
+module.exports = Banner;
