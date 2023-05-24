1300879e69ad810e364f4ee2fd329bec64663ba3
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-24 12:33:09 +0545

    Refactored APIFeatures class in a separate file

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,71 +2,7 @@
 const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
-
-class APIFeatures {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
-    }
-
-    filter() {
-        const queryObj = { ...this.queryString };
-
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-
-        // Removing some extra fields from query string
-        for (const key of Object.keys(queryObj)) {
-            if (excludedFields.includes(key)) {
-                delete queryObj[key];
-            }
-        }
-
-        // Advanced Filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(
-            /\b(gte|gt|lte|lt)\b/g,
-            (match) => `$${match}`);
-
-        this.query.find(JSON.parse(queryStr));
-
-        return this;
-    }
-
-    sort() {
-        // Sorting
-        if (this.queryString.sort) {
-            console.log(this.queryString.sort);
-            const sortBy = this.queryString.sort.split(',').join(' ');
-            this.query = this.query.sort(sortBy);
-        }
-        else {
-            this.query = this.query.sort('-createdAt');
-        }
-        return this;
-    }
-
-    limitFields() {
-        // Field limiting
-        if (this.queryString.fields) {
-            const fields = this.queryString.fields.split(",").join(" ");
-            this.query = this.query.select(fields);
-        } else {
-            this.query = this.query.select("-__v");
-        }
-        return this;
-    }
-
-    pagniate() {
-        // Pagination
-        const page = +this.queryString.page || 1;
-        const limit = +this.queryString.limit || 100;
-        const skip = (page - 1) * limit;
-
-        this.query = this.query.skip(skip).limit(limit);
-
-        return this;
-    }
-}
+const APIFeatures = require('./../utils/APIFeatures');
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const features = new APIFeatures(User.find(), req.query).filter().sort().limitFields().pagniate();
