15e88656af84d871185c110438fc8f4458ae3c92
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-30 20:27:35 +0545

    Added a nested create routes for reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -33,7 +33,12 @@ exports.getReview = catchAsync(async (req, res, next) => {
 
 
 exports.createReview = catchAsync(async (req, res, next) => {
+
+    if (!req.body.product) req.body.product = req.params.productID;
+    if (!req.body.user) req.body.user = req.user.id;
+
     const newReview = await Review.create(req.body);
+
 
     res.status(201).json({
         status: 'success',
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const authController = require('./../controllers/authController');
 const productController = require('./../controllers/productController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:productID/reviews', reviewRouter);
 
 router.route('/').get(productController.getAllProducts).post(authController.protect, authController.restrictTo('admin', 'seller'), productController.uploadProductImages, productController.resizeProductImages, productController.createProduct);
 router.route('/:id').get(productController.getProduct).patch(authController.protect, authController.restrictTo('admin', 'seller'), productController.uploadProductImages, productController.resizeProductImages, productController.updateProduct).delete(authController.protect, authController.restrictTo('admin', 'seller'), productController.deleteProduct);
