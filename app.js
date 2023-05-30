80e97d0634b864017bb584826520b2e33f50d1a9
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-30 17:11:21 +0545

    Configured routing for reveiws

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bannerRouter = require('./routes/bannerRoutes');
 const categoryRouter = require('./routes/categoryRoutes');
 const brandRouter = require('./routes/brandRoutes');
 const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const userRouter = require('./routes/userRoutes');
 
 
@@ -24,6 +25,7 @@ app.use('/api/v1/banners', bannerRouter);
 app.use('/api/v1/brands', brandRouter);
 app.use('/api/v1/categories', categoryRouter);
 app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/users', userRouter);
 
 app.use('*', (req, res, next) => {
