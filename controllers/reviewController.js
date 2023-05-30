197b9675716ebe51f284c9e7b4a943a16b98f95d
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-30 17:07:07 +0545

    Added CRUD functionality for reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,73 @@
+const Review = require('./../models/reviewModel');
+
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
+
+exports.getAllReveiws = catchAsync(async (req, res, next) => {
+
+    const features = new APIFeatures(Review.find(), req.query).filter().limitFields().pagniate().sort();
+    const reviews = await features.query;
+
+    res.status(200).json({
+        status: 'success',
+        results: reviews.length,
+        data: {
+            reviews,
+        }
+    })
+})
+
+exports.getReview = catchAsync(async (req, res, next) => {
+    const review = await Review.findById(req.params.id);
+
+    if (!review) return next(new AppError("No review found with that ID!", 404));
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            review
+        }
+    })
+})
+
+
+exports.createReview = catchAsync(async (req, res, next) => {
+    const newReview = await Review.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            review: newReview
+        }
+    })
+})
+
+
+exports.updateReview = catchAsync(async (req, res, next) => {
+    const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    if (!review) return next(new AppError("No review found with that ID!", 404));
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            review
+        }
+    })
+})
+
+
+exports.deleteReview = catchAsync(async (req, res, next) => {
+    const review = await Review.findByIdAndDelete(req.params.id);
+
+    if (!review) return next(new AppError("No review found with that ID!", 404));
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
