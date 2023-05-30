cca06a8a738f741511a6e36de9ef6a207f05910d
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-30 16:38:17 +0545

    Edited the authorization and authentication in product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,9 +5,7 @@ const productController = require('./../controllers/productController');
 
 const router = express.Router();
 
-router.use(authController.protect, authController.restrictTo('admin'));
-
-router.route('/').get(productController.getAllProducts).post(productController.uploadProductImages, productController.resizeProductImages, productController.createProduct);
-router.route('/:id').get(productController.getProduct).patch(productController.uploadProductImages, productController.resizeProductImages, productController.updateProduct).delete(productController.deleteProduct);
+router.route('/').get(productController.getAllProducts).post(authController.protect, authController.restrictTo('admin', 'seller'), productController.uploadProductImages, productController.resizeProductImages, productController.createProduct);
+router.route('/:id').get(productController.getProduct).patch(authController.protect, authController.restrictTo('admin', 'seller'), productController.uploadProductImages, productController.resizeProductImages, productController.updateProduct).delete(authController.protect, authController.restrictTo('admin', 'seller'), productController.deleteProduct);
 
 module.exports = router;
