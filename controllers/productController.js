9496e7a65d733c3df39bfca50215e63e764487be
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-30 15:54:37 +0545

    Added a delete product middleware for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -94,6 +94,17 @@ exports.updateProduct = catchAsync(async (req, res, next) => {
     })
 })
 
+exports.deleteProduct = catchAsync(async (req, res, next) => {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) return next(new AppError("No product found with that ID!", 404));
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+
 // exports.deleteCategory = catchAsync(async (req, res, next) => {
 //     const category = await Category.findByIdAndDelete(req.params.id);
 
