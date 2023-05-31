29dfd355f0e7ab7ca3d6669ed08d331cd62db219
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-31 11:23:37 +0545

    Added fields for ratingsAverage and ratingsQuantity in product

--- models/productModel.js
+++ models/productModel.js
@@ -18,6 +18,17 @@ const productSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        min: [1, "Rating must be above 1.0"],
+        max: [5, "Rating must be below 5.0"],
+        set: val => Math.round(val * 10) / 10
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
     categories: [{
         type: mongoose.Schema.ObjectId,
         ref: 'Category',
