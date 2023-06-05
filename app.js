2201348528967b2050e9d136cf61cd3d6171cd91
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-06-05 11:15:37 +0545

    Added different security in the app

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,13 +15,33 @@ const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const userRouter = require('./routes/userRoutes');
 
-
-
 const app = express();
+
+// Helmet
+app.use(helmet());
 
 // JSON parser middleware with size limit of 10kb
 app.use(express.json({ limit: '10kb' }));
 
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+})
+
+// Rate Limiter
+app.use('/api', limiter);
+
+// Data Sanitization against NoSql query injection
+app.use(mongoSanitize());
+
+// Data sanitizaiton against xss
+app.use(xss());
+
+// Avoiding parameter pollution: Must be used in last because it clears query string
+app.use(hpp({
+    whitelist: ['price', 'ratingsQuantity', 'ratingsAverage']
+}))
 
 // middlewares for serving static files
 app.use(express.static(`${__dirname}/public`));
